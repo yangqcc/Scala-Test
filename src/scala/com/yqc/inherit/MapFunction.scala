87e64b6ba06b4b128dc3adf70d888c3c7e557cb6
yangqc <yangqc_ars@outlook.com>
2017-04-19 23:35:04 +0800

    scala函数

--- src/scala/com/yqc/inherit/MapFunction.scala
+++ src/scala/com/yqc/inherit/MapFunction.scala
@@ -4,5 +4,11 @@ package com.yqc.inherit
   * Created by yangqc on 2017/4/19.
   */
 object MapFunction {
-
+  def main(args: Array[String]): Unit = {
+    var myArray: Array[String] = Array[String]("spark", "hive", "hadoop")
+    var anotherArray: Array[String] = myArray.map((x: String) => x * 2)
+    for (x <- anotherArray) {
+      println(x)
+    }
+  }
 }
