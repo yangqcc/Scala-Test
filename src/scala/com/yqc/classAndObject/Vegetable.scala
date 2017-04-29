5bc17567a195a885098d5029120e57bf8d8391eb
yangqc <yangqc_ars@outlook.com>
2017-04-29 11:57:42 +0800

    伴生类

--- src/scala/com/yqc/classAndObject/Vegetable.scala
+++ src/scala/com/yqc/classAndObject/Vegetable.scala
@@ -3,12 +3,22 @@ package com.yqc.classAndObject
 /**
   * Created by yangqc on 2017/4/22.
   */
-abstract class Vegetable(var name: String) {
+abstract class Vegetable {
 
   //一般类中定义字段的话必须初始化,而抽象类中则没有这要求
   var height: Int
 
   def eat: Unit
+
+  def this(name: String) = {
+    this()
+    println("我是构造函数!")
+  }
+
+  def this(height: Int) = {
+    this();
+    this.height = height
+  }
 }
 
 class MyVegetable(name: String) extends Vegetable(name) {
@@ -24,6 +34,21 @@ object MyVegetable {
 
   def main(args: Array[String]): Unit = {
     MyVegetable("Apple").eat
+  }
+}
+
+object Vegetable2 extends Vegetable {
+
+  override var height: Int = 12
+
+  override def eat: Unit = println(height)
+
+  def showHeight() = {
+    println(height)
+  }
+
+  def main(args: Array[String]): Unit = {
+    Vegetable2.showHeight()
   }
 }
 
