29d9d5ada8cc99c1d4735b1d95516ff2a94b5b54
yangqc <yangqc_ars@outlook.com>
2017-05-14 00:19:41 +0800

    Scala单例伴生类

---
+++ src/scala/com/yqc/singletonObject/SingletonObject.scala
@@ -0,0 +1,30 @@
+package com.yqc.singletonObject
+
+/**
+  * Created by yangqc on 2017/5/14.
+  *
+  * 单例伴生类
+  */
+class SingletonObject private {
+
+  var field = "field"
+
+  def doSomething: Unit = {
+    println("do something")
+  }
+}
+
+object SingletonObject {
+  val single = new SingletonObject
+  val a = "a String"
+
+  def printString: Unit = {
+    println(a)
+  }
+
+  def main(args: Array[String]): Unit = {
+    var a = SingletonObject.single
+    var b = SingletonObject.single
+    println("a eq b : " + (a eq b))
+  }
+}
