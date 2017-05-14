c2a87ecf656a2ef84ee17fba460639bf6bb968ce
yangqc <yangqc_ars@outlook.com>
2017-05-15 00:40:46 +0800

    修改类名

--- src/scala/com/yqc/functionModel/TestRational.scala
+++ src/scala/com/yqc/functionModel/TestRational.scala
@@ -3,7 +3,7 @@ package com.yqc.functionModel
 /**
   * Created by yangqc on 2017/5/15.
   */
-object RationalTest {
+object TestRational {
   def main(args: Array[String]): Unit = {
     val oneHalf = new Rational(1, 2)
     val twoThirds = new Rational(2, 3)
