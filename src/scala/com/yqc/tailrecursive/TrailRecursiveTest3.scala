d6128e7a99516846983bbf310292f38ccc6cfdd4
yangqc <yangqc_ars@outlook.com>
2017-05-24 23:43:56 +0800

    scala 尾递归局限

--- src/scala/com/yqc/tailrecursive/TrailRecursiveTest3.scala
+++ src/scala/com/yqc/tailrecursive/TrailRecursiveTest3.scala
@@ -1,5 +1,7 @@
 package com.yqc.tailrecursive
 
+import scala.annotation.tailrec
+
 /**
   * Created by yangqc on 2017/5/24.
   *
@@ -20,12 +22,14 @@ object TrailRecursiveTest3 {
 
   /**
     * 斐波那契数列转化为尾递归
+    * {@code tailrec}用于标识是否是尾递归
     *
     * @param n
     * @param acc1
     * @param acc2
     * @return
     */
+  @tailrec
   def fibonacciTailRecursive(n: Int, acc1: Int, acc2: Int): Int = {
     if (n < 2) acc2
     else fibonacciTailRecursive(n - 1, acc2, acc1 + acc2)
