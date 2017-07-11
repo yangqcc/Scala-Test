2b8ff5c79ca295736ce0e94766e93fbc462e198d
yangqc <yangqc_ars@outlook.com>
2017-07-11 22:55:59 +0800

    scala逆变和协变

--- src/scala/com/yqc/covariance/OutputChannel.scala
+++ src/scala/com/yqc/covariance/OutputChannel.scala
@@ -3,6 +3,21 @@ package com.yqc.covariance
 /**
   * Created by yangqc on 2017/7/11.
   */
+class Animal {
+  override def toString: String = "this is animal!"
+}
+
+class Dog extends Animal {
+  override def toString(): String = "this is Dog!"
+}
+
+class Cat extends Dog {
+  override def toString(): String = "this is Cat!"
+}
+
+class Pig extends Cat {
+  override def toString(): String = "this is Pig!"
+}
 
 class OutputChannel[-T] {
 
@@ -15,20 +30,20 @@ class OutputChannel[-T] {
   def write[U <: T](x: U): Unit = println(x)
 }
 
-class ChildClass extends OutputChannel[Any] {
-  /**
-    * 使用T的子类U来替换T
-    *
-    * @param x
-    * @tparam U
-    */
-  override def write[U <: Any](x: U): Unit = println(x)
+class MyAnimal[+A] {
+
+  def showName[U >: A](x: U) = println(x)
 }
 
 object Test {
+
   def main(args: Array[String]): Unit = {
-    var outputChannel: OutputChannel[Any] = new OutputChannel[Any]
-    outputChannel = new ChildClass
-    outputChannel.write("qicheng")
+    var outputChannel: OutputChannel[Dog] = new OutputChannel[Dog]
+    outputChannel = new OutputChannel[Animal]
+    outputChannel.write(new Cat)
+
+    var myAnimal: MyAnimal[Cat] = new MyAnimal[Cat]
+    myAnimal = new MyAnimal[Pig]
+    myAnimal.showName(new Animal)
   }
 }
